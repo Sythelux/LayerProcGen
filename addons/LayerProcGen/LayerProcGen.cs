43b4da87af7f0a202eb861e3833c51d7e378867a
Sythelux Rikd <dersyth@gmail.com>
2024-05-29 20:37:57 +0200

    fixed Godot specific things.
    Added only halfway working _Get and _Set in editor.
    
    Added extra line at the end in some files

--- addons/LayerProcGen/LayerProcGen.cs
+++ addons/LayerProcGen/LayerProcGen.cs
@@ -1,15 +1,7 @@
 using Godot;
 using System;
 
-public partial class LayerProcGen : Node
+public partial class LayerProcGen : EditorPlugin
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    //nothing to do for now.
 }
